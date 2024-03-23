5cff33eb7314c9e2ad741b432e5df2bd1eb8a9a9
Alviora <159285811+0xAlviora@users.noreply.github.com>
2024-03-23 16:59:37 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-464352e5-cd46-47d6-b31c-699064ad3251//)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-464352e5-cd46-47d6-b31c-699064ad3251/)
 
 
 echo "$curl_output" >> Update.tsx
